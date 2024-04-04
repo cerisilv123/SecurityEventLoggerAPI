6bf9bfc6121bf39e3f723dfe4bbcc9c89fff2f8f
Ceri Silvester <cerisilv@hotmail.co.uk>
2024-04-04 16:04:51 +0100

    first commit - added database Models Category and Event.

---
+++ SecurityEventLoggerAPI/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace SecurityEventLoggerAPI.Models
+{
+    public class Category
+    {
+        public Guid Id { get; set; } = Guid.NewGuid(); // Automatically generates a new GUID for primary key
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
---
+++ SecurityEventLoggerAPI/Models/Event.cs
@@ -0,0 +1,21 @@
+﻿namespace SecurityEventLoggerAPI.Models
+{
+    public class Event
+    {
+        public Guid Id { get; set; } = Guid.NewGuid(); // Automatically generates a new GUID for primary key
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Location { get; set; }
+
+        public int Severity { get; set; }
+
+        public string Status { get; set; }
+
+        public string? Resolution { get; set; }
+
+        public DateTime EventDateTime { get; set; }
+    }
+}
